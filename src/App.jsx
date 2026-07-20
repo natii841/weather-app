a857ef4ba7ffaf003b277b31e99c4b16a98de435
natii841 <natiyehiwotlij@gmail.com>
2026-07-21 01:55:45 +0300

    dummy-ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 const App = () => {
   return (
-    <div className="text-center">Hello React!</div>
+    <>
+    
+     <div className="text-center bg-amber-900 container lg:mx-auto px-6 py-12 shadow-2xl sm:mx-auto max-w-5xl">
+      <h1 className="text-4xl font-extrabold text-white">Hello React!</h1>
+      <p className="mt-4 text-lg text-white/80">Leykun and Natanim 💪</p>
+     </div>
+    
+    </>
+   
   )
 }
 
